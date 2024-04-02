2b1cfc7847a9f3fee2061a53ba5d06f92ed99696
qReol <stim19255@gmail.com>
2024-04-02 08:53:58 +0500

    #9 Add slug convertor for categories and products.

--- goods/views.py
+++ goods/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 from goods.models import Products
 
 
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
 
     context: dict = {
         'title': 'Home - Каталог',
@@ -14,5 +17,12 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'title': product.name,
+        'product': product,
+    }
+
+    return render(request, 'goods/product.html', context)
